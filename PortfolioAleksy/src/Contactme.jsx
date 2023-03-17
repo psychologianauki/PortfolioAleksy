83808290c47307bb36bd49a2e3171f12a3c249fc
Aleksy <psychologianauki@gmail.com>
2023-03-17 23:20:59 +0100

    a few more icons, footer is done, with links to socials

--- PortfolioAleksy/src/Contactme.jsx
+++ PortfolioAleksy/src/Contactme.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function Contactme() {
-  return <div>Contactme</div>;
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className="header">Contact </div>
+      </div>
+      <form>
+        <div>Twój e-mail*</div>
+
+        <input
+          className="forminput"
+          type="email"
+          placeholder="enter your email"
+          style={{
+            width: "50%",
+            borderColor: "transparent",
+          }}
+        ></input>
+        <div>Your message</div>
+        <div>
+          <textarea
+            className="formmessage"
+            rows="6"
+            placeholder="enter your message"
+          ></textarea>
+        </div>
+        <button>Sent!</button>
+      </form>
+    </div>
+  );
 }
