ed68cbb0b5b2ccb97d482506a55530fca4784258
Aleksy <psychologianauki@gmail.com>
2023-03-18 12:51:37 +0100

    added pict to portfolio, not styled yet

--- PortfolioAleksy/src/Portfolio.jsx
+++ PortfolioAleksy/src/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import portfolioBackground from "./assets/image/portfolioBackground.jpg";
+import ToDoList from "./assets/image/toDoList.png";
+import gielda from "./assets/image/gielda.png";
 
 export default function Portfolio() {
   return (
@@ -11,6 +13,23 @@ export default function Portfolio() {
           className="overlay"
         >
           <div className="header">PORTFOLIO</div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "black",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div className="overlay2" style={{ height: "100px" }}>
+            <img src={ToDoList} style={{ height: "200px" }}></img>
+          </div>
+          <div className="overlay2">
+            {" "}
+            <img src={gielda} style={{ height: "200px", left: "100px" }}></img>
+          </div>
         </div>
       </div>
     </div>
